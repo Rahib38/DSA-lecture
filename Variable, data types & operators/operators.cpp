8b0ceccb2634ca37c0766896e63e4a4b2555adf4
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-12-06 07:26:10 -0800

    operators

---
+++ Variable, data types & operators/operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a, b;
+    int sum;
+    cout << "Enter the 1st number: ";
+    cin >> a;
+    cout << "Enter the 2nd number: ";
+    cin >> b;
+
+    sum = a + b;
+    cout << "Summation is: " << sum << endl;
+
+    return 0;
+}
