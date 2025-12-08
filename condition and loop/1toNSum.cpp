c1b00516ccc87bb551d15dae443bc9ce4c34ab28
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-12-08 08:06:20 -0800

    1 to N summation

---
+++ condition and loop/1toNSum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int sum = 0;
+    int n;
+
+    cout << "Enter the number ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        sum += i;
+    }
+    cout << "The Summation is: " << sum;
+
+    return 0;
+}
