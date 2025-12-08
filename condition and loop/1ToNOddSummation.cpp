4969f8e190288d15aed2d11c0fe2441d810b071e
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-12-08 08:48:41 -0800

    while loop

--- condition and loop/1ToNOddSummation.cpp
+++ condition and loop/1ToNOddSummation.cpp
@@ -6,16 +6,29 @@ int main()
 
     int sum = 0;
     int n;
+    int i =1;
 
     cout << "Enter the number ";
     cin >> n;
 
-    for (int i = 1; i <= n; i+=2)
-    {
+    // for (int i = 1; i <= n; i+=2)
+    // {
 
-        sum += i;
+    //     sum += i;
+    // }
+
+    while ( i<=n)
+    {
+        /* code */
+        if(i%2==0){
+            sum+=i;
+        }
+        i++;
+        
     }
     cout << "The Summation is: " << sum;
+    
+
 
     return 0;
 }
